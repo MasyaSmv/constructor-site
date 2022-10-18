f17f9bfacccc83bbf6e2edd26fc5e6023709dc00
Masya Smerdov <61746307+masyasmv@users.noreply.github.com>
2022-10-19 00:46:33 +0300

    add react and TS

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
+import Home from "./components/home";
+import React from "react";
+import ReactDOM from "react-dom";
+
 require('./bootstrap');
+
+
+ReactDOM.render(
+  <span><Home /></span>,
+    document.querySelector('#root')
+);
---
+++ resources/js/components/home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Home = () => {
+    let foo: string = 'Reactsss';
+    const bar: string = 'TP';
+
+    return (
+        <h1>
+            {foo} + {bar}
+        </h1>
+    )
+};
+
+export default Home;
